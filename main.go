43db85bf45e1eab25c80e91025c1969d55c4229d
“Rahul” <rahulmanu7902@email.com>
2023-04-24 14:30:38 +0530

    main.go

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import(
+	"fmt"
+	"log"
+	"net/http"
+)
+
+
+
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+if r.URL.Path != "/hello"{
+http.Error(w, "404 not found", http.StatusNotFound)
+return
+}
+if r.Method != "GET"{
+	http.Error(w, "method not allowed", http.StatusFound)
+}
+
+fmt.Fprintf(w, "Hello World!")
+}
+
+
+func formHandler(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil{
+		fmt.Fprintf(w, "ParseForm() err:%v ",err)
+	}
+	fmt.Fprintf(w,"POST request is successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w,"Name:%s\n",name)
+	fmt.Fprintf(w,"Email=%s\n",address)
+}
+
+func main() {
+fileSever := http.FileServer(http.Dir("./static"))
+http.Handle("/",fileSever)
+http.HandleFunc("/form",formHandler)
+http.HandleFunc("/hello",helloHandler)
+
+
+fmt.Printf("server listening at port : 8080\n")
+if err := http.ListenAndServe(":8080", nil); err != nil {
+log.Fatal(err)
+}
+
+}
